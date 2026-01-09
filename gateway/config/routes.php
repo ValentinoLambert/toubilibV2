469ca5d9c7dd78942ad9f449bfb798cdbeceb598
e7160u <valentino.lambert94@gmail.com>
2026-01-09 15:22:11 +0100

    praticien/id

--- gateway/config/routes.php
+++ gateway/config/routes.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 use Slim\App;
 use toubilib\gateway\application\actions\GetPraticiensAction;
+use toubilib\gateway\application\actions\GetPraticienAction;
 
 return function (App $app) {
     $app->get('/praticiens', GetPraticiensAction::class);
+    $app->get('/praticiens/{id}', GetPraticienAction::class);
 };
