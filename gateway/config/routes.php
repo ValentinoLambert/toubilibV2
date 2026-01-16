659fb4d1464d9622e82257138b67165a977e81cf
e7160u <valentino.lambert94@gmail.com>
2026-01-16 14:52:50 +0100

    ex1

--- gateway/config/routes.php
+++ gateway/config/routes.php
@@ -3,13 +3,7 @@ declare(strict_types=1);
 
 use Slim\App;
 use toubilib\gateway\application\actions\ProxyAction;
-use toubilib\gateway\application\actions\ProxyPraticiensAction;
-use toubilib\gateway\application\actions\ProxyRdvAction;
 
 return function (App $app) {
-    $app->get('/praticiens/{id}/agenda', ProxyRdvAction::class);
-    $app->get('/praticiens/{id}/rdv/occupes', ProxyRdvAction::class);
-    $app->map(['GET', 'POST', 'PATCH', 'DELETE'], '/rdv[/{routes:.*}]', ProxyRdvAction::class);
-    $app->get('/praticiens[/{routes:.*}]', ProxyPraticiensAction::class);
     $app->map(['GET', 'POST', 'PATCH', 'DELETE'], '/{routes:.+}', ProxyAction::class);
 };
